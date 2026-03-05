401f0234aee577e25506c0d327d02b920be4356e
Travis <Travisang40@gmail.com>
2026-03-05 10:26:04 +0800

    updated website icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ const serif = Jua({
 export const metadata: Metadata = {
   title: "Travis Ang",
   description: "Developer Portfolio website of Travis Ang",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>",
+  },
 };
 
 export default function RootLayout({
