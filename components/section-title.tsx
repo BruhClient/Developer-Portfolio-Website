7c2909962f6faf73088f57d5ca29675608a6b670
Travis <Travisang40@gmail.com>
2026-04-23 14:03:26 +0800

    updated header

--- components/section-title.tsx
+++ components/section-title.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const SectionTitle = ({ title }: { title: string }) => {
   return (
-    <div className="text-lg text-primary" id={title}>
-      {title}
-    </div>
+    <div className="text-lg text-primary" id={title}>{`< ${title} /> `}</div>
   );
 };
 
